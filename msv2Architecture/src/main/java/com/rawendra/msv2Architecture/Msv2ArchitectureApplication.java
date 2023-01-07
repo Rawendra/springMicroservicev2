d1c00d109983b6cf7b2bf2fbc7545d67ce5a744f
rawendra <rawendra@gmail.com>
2023-01-08 01:24:21 +0530

    first commit

---
+++ msv2Architecture/src/main/java/com/rawendra/msv2Architecture/Msv2ArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.rawendra.msv2Architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msv2ArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Msv2ArchitectureApplication.class, args);
+	}
+
+}
---
+++ msv2Architecture/src/test/java/com/rawendra/msv2Architecture/Msv2ArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rawendra.msv2Architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Msv2ArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
